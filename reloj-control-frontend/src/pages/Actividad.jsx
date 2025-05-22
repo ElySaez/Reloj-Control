2efb1689584c2564c5d1c3ed6e35dba9385a2665
ElySaez <eesaez.munoz@gmail.com>
2025-05-22 13:14:12 -0400

    Cambio a placeholders

--- reloj-control-frontend/src/pages/Actividad.jsx
+++ reloj-control-frontend/src/pages/Actividad.jsx
@@ -77,7 +77,7 @@ export default function ActividadPage() {
                             id="runBusquedaInput"
                             value={runBusqueda}
                             onChange={(e) => setRunBusqueda(e.target.value)}
-                            placeholder="Ingrese RUN (ej: 12345678-9)"
+                            placeholder="Ingrese RUN (ej: 12345678)"
                             className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                             disabled={loading}
                         />
--- reloj-control-frontend/src/pages/LoginPage.jsx
+++ reloj-control-frontend/src/pages/LoginPage.jsx
@@ -130,7 +130,7 @@ export default function LoginPage() {
                                 autoComplete="username" // Opcional, para gestores de contraseñas
                                 required
                                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                                placeholder="RUN (Ej: 12345678-9)"
+                                placeholder="RUN (Ej: 12345678)"
                                 value={run}
                                 onChange={(e) => setRun(e.target.value)}
                                 disabled={loading}
